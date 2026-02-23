73af12a395670287b8b28ed632c767d25f95fceb
ToastedRoast <ronan.alouges@gmail.com>
2026-02-23 19:09:33 +0100

    Initial commit

---
+++ src/main/java/com/yelo/blockbeats/BlockBeats.java
@@ -0,0 +1,15 @@
+package com.yelo.blockbeats;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlockBeats implements ModInitializer {
+	public static final String MOD_ID = "blockbeats";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/yelo/blockbeats/BlockBeatsClient.java
@@ -0,0 +1,10 @@
+package com.yelo.blockbeats;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockBeatsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
